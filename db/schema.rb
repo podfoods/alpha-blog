38d4f257975f53be97d61b9d3cf9e4d48aa97d3c
Rails Course <railscourse@gmail.com>
2017-05-29 00:03:50 +0000

    user validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528184426) do
+ActiveRecord::Schema.define(version: 20170528231930) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20170528184426) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
